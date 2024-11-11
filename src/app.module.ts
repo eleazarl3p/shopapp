c16544efebaf5815d7154bccd4822ea942ba3e7f
egeneste <98227174+eleazarl3p@users.noreply.github.com>
2024-11-11 11:05:36 -0500

    add databse connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [],
+      autoLoadEntities: true,
+      synchronize: true,
+      timezone: 'Z',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
