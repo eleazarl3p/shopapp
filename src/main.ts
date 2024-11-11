783f539a4fed1fba3b21111504fca129723cdbfa
egeneste <98227174+eleazarl3p@users.noreply.github.com>
2024-11-11 11:21:39 -0500

    add validator

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+  process.env.TZ = 'America/Santo_Domingo';
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
