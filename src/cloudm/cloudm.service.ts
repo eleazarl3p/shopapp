90680baf53fc690f2ce8a903080e95fdeaddcb89
egeneste <98227174+eleazarl3p@users.noreply.github.com>
2024-11-11 20:57:02 -0500

    remove auth

--- src/cloudm/cloudm.service.ts
+++ src/cloudm/cloudm.service.ts
@@ -6,10 +6,10 @@ export class CloudmService {
   private driveClient;
 
   constructor() {
-    const auth = new google.auth.GoogleAuth({
-      keyFile: process.env.GOOGLE_APPLICATION_CREDENTIALS,
-      scopes: ['https://www.googleapis.com/auth/drive.file'],
-    });
-    this.driveClient = google.drive({ version: 'v3', auth });
+    // const auth = new google.auth.GoogleAuth({
+    //   keyFile: process.env.GOOGLE_APPLICATION_CREDENTIALS,
+    //   scopes: ['https://www.googleapis.com/auth/drive.file'],
+    // });
+    // this.driveClient = google.drive({ version: 'v3', auth });
   }
 }
