f68fa1e5ae02857905ceb3c132fb3c12f662916d
egeneste <98227174+eleazarl3p@users.noreply.github.com>
2024-11-11 21:12:30 -0500

    lazy import googleapi

--- src/cloudm/cloudm.service.ts
+++ src/cloudm/cloudm.service.ts
@@ -1,15 +1,27 @@
 import { Injectable } from '@nestjs/common';
-// import { google } from 'googleapis';
 
 @Injectable()
 export class CloudmService {
   private driveClient;
 
   constructor() {
-    // const auth = new google.auth.GoogleAuth({
-    //   keyFile: process.env.GOOGLE_APPLICATION_CREDENTIALS,
-    //   scopes: ['https://www.googleapis.com/auth/drive.file'],
-    // });
-    // this.driveClient = google.drive({ version: 'v3', auth });
+    // Drive client is not initialized immediately, it will be loaded lazily
+    this.driveClient = null;
+  }
+
+  // Lazy initialize the Google Drive client
+  private async initDriveClient() {
+    if (!this.driveClient) {
+      // Dynamically import 'googleapis' only when needed
+      const { google } = await import('googleapis');
+
+      const auth = new google.auth.GoogleAuth({
+        keyFile: process.env.GOOGLE_APPLICATION_CREDENTIALS,
+        scopes: ['https://www.googleapis.com/auth/drive.file'],
+      });
+
+      this.driveClient = google.drive({ version: 'v3', auth });
+    }
+    return this.driveClient;
   }
 }
